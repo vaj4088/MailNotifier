408ddc39368d0e74300bd2d7f49fa0a941ba6821
Ian Shef vaj4088 <vaj4088@Coyote2021Linux>
2022-10-02 15:19:57 -0500

    Create Git repo and snapshot 20221002.

---
+++ README.md
@@ -0,0 +1,2 @@
+# MailNotifier
+Access a web site when mail arrives.  Aiden's Home Assistant home automation will send a messages when the home automation web site is accessed.
---
+++ src/SSIDprivate.h
@@ -0,0 +1,57 @@
+
+// Constants won't change:
+
+//const char* ssid     = "*" ; // Replace * by the name (SSID) for your network.
+//const char* password = "*" ; // Replace * by the password    for your network.
+
+// Constants won't change:
+
+#if defined Home
+
+// ssid =     "Brian's Wireless Root" ;
+// password = "valley4588" ;
+
+const char* ssid     = "Bskdn(u#Wjthlfuv Sqrt" ;
+ 
+const char* password = "vbnoez6889" ;
+
+#elif defined Aiden
+
+// ssid =     "APDCHQ" ;
+// password = "99robusttidepools" ;
+
+const char* ssid     = "AQFFHR" ;
+
+const char* password = "9:trbvuwtjfhppqos" ;
+
+#endif
+
+// https://maker.ifttt.com/trigger/Mail_Notifier/with/key/dYLX3VKulOc56HPyOwWHIi?value1=3.39%20(8/11/2022%2017:34)
+
+const char * triggerRequest=
+  "/trigger/Mail_Notifier/with/key/dYLX3VKulOc56HPyOwWHIi?value1=" ;
+
+// const char * triggerRequest=
+//   "/trigger/Mail_Notifier/with/key/cDzWK_pUFYz81CZn1CmLWS?value1=" ;
+
+// const char * triggerRequest=
+//   "/trigger/Mail_Notifier/with/key/bbFkrc2AzZkrcNH8OkyBIn?value1=" ;
+
+// const char * triggerRequest=
+//   "/trigger/Mail_Notifier/with/key/bBzMt3GMKR46GbTLP6v919?value1=" ;
+
+/*
+
+Input:  APDCHQ  0X41 0X50 0X44 0X43 0X48 0X51
+Output: AQFFHR {  0X41, 0X51, 0X46, 0X46, 0X48, 0X52}
+
+Input:  99robusttidepools  0X39 0X39 0X72 0X6F 0X62 0X75 0X73 0X74 0X74 0X69 0X64 0X65 0X70 0X6F 0X6F 0X6C 0X73
+Output: 9:trbvuwtjfhppqos {  0X39, 0X3A, 0X74, 0X72, 0X62, 0X76, 0X75, 0X77, 0X74, 0X6A, 0X66, 0X68, 0X70, 0X70, 0X71, 0X6F, 0X73}
+
+Input:  Brian's Wireless Root  0X42 0X72 0X69 0X61 0X6E 0X27 0X73 0X20 0X57 0X69 0X72 0X65 0X6C 0X65 0X73 0X73 0X20 0X52 0X6F 0X6F 0X74
+Output: Bskdn(u#Wjthlfuv Sqrt {  0X42, 0X73, 0X6B, 0X64, 0X6E, 0X28, 0X75, 0X23, 0X57, 0X6A, 0X74, 0X68, 0X6C, 0X66, 0X75, 0X76, 0X20, 0X53, 0X71, 0X72, 0X74}
+
+Input:  valley4588  0X76 0X61 0X6C 0X6C 0X65 0X79 0X34 0X35 0X38 0X38
+Output: vbnoez6889 {  0X76, 0X62, 0X6E, 0X6F, 0X65, 0X7A, 0X36, 0X38, 0X38, 0X39}
+
+*/
